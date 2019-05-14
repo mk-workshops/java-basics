b294172c42eb90a5940d46e9687250000a93f2e0
Michał Kopacz <michalkopacz.mk@gmail.com>
2019-05-14 14:42:04 +0200

    init

---
+++ src/test/java/mk/workshops/moneyconverter/Hello.java
@@ -0,0 +1,11 @@
+package mk.workshops.moneyconverter;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+public class Hello {
+    @Test
+    public void alwaysTrue() {
+        Assertions.assertThat(true).isTrue();
+    }
+}
